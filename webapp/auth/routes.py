d1ce4d853a98b81c5c9b3b2451e33b45c0bf8829
SusanDz <sd2009@hw.ac.uk>
2023-01-23 22:16:35 +0400

    backend logic for user logging in

--- webapp/auth/routes.py
+++ webapp/auth/routes.py
@@ -1,13 +1,39 @@
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
+from models import User
+from werkzeug.security import check_password_hash
 from webapp.auth import auth
+from flask_login import login_user
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('base.html')
+    if request.method == 'POST':
+        # get username and password from form
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-@auth.route('/signup')
+        # user is returned then the user with given username exists
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            # for particular user check if the password provided matches with user in database
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+
+                # User is logged in, remember user even after session expires
+                login_user(user, remember=True)
+
+                # redirect to function home of the route '/home'
+                return redirect(url_for('profile.home'))
+            else:
+                flash('Password entered is incorrect.', category='error')
+        else:
+            flash('Email provided is incorrect.', category='error')
+
+    return render_template('login.html')
+
+@auth.route('/register')
 def signup():
-    return 'Signup'
+    return render_template('register.html')
 
 @auth.route('/logout')
 def logout():
