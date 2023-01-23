d42ee11324cc03975aa3058e8ffd1bd254b613f9
SusanDz <sd2009@hw.ac.uk>
2023-01-23 17:37:27 +0400

    create a model for user

---
+++ webapp/auth/models.py
@@ -0,0 +1,7 @@
+from .. import db
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    description = db.Column(db.String(150))
