13abe0113dc4c99aca2342a71421398bf0ef81a5
SusanDz <sd2009@hw.ac.uk>
2023-01-23 22:17:21 +0400

    changed class name

--- webapp/auth/models.py
+++ webapp/auth/models.py
@@ -1,5 +1,5 @@
 from .. import db
-class Users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
     email = db.Column(db.String(150), unique=True)
