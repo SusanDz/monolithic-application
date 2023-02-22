0c5bcebdbab9714558b20d1053721a78df6018cd
SusanDz <sd2009@hw.ac.uk>
2023-02-22 23:29:14 +0400

    send dummy data to html page

--- webapp/order_page/routes.py
+++ webapp/order_page/routes.py
@@ -4,4 +4,5 @@ from webapp.order_page import order_page as order
 @order.route('/order', methods=['GET', 'POST'])
 def order():
     #get data from send as variable key value pair
-    return render_template('order.html')
+    data = [{'_id': 1, 'name': 'Banana', 'price': 15}, {'_id': 2, 'name': 'Pie', 'price': 15}]
+    return render_template('order.html', data=data)
