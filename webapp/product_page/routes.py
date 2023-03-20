8f378268e1556f706928ddc45f07955e8f21250d
SusanDz <sd2009@hw.ac.uk>
2023-03-20 23:55:13 +0400

    create methods to handle get product and add product routes

--- webapp/product_page/routes.py
+++ webapp/product_page/routes.py
@@ -3,31 +3,17 @@ from webapp.product_page import product_page as product
 
 @product.route('/products', methods=['GET', 'POST'])
 def products():
-    if request.method == 'POST':
-        # get username and password from form
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        print(username)
-
-        # user is returned then the user with given username exists
-        user = User.query.filter_by(username=username).first()
-
-        if user:
-            # for particular user check if the password provided matches with user in database
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-
-                # User is logged in, remember user even after session expires
-                login_user(user, remember=True)
-
-                # redirect to function home of the route '/home'
-                return redirect(url_for('profile.home'))
-            else:
-                # If username and password doesn't match flash message
-                flash('Username or Password entered is incorrect.', category='error')
-        else:
-            # MIGHT REMOVE THIS CONDITION
-            flash('Credentials entered is incorrect. Create an account instead!', category='error')
-
+    #get products from db and send to to html
     return render_template('products.html')
+
+@product.route('/addProduct', methods=['GET', 'POST'])
+def addProduct():
+    if request.method == 'POST':
+        # get name and price and picture from form
+        name = request.form.get('name')
+        price = request.form.get('price')
+        picture = request.form.get('picture')
+
+        flash('Created product succesfully!', category='success')
+        
+    return render_template('addProduct.html')
