d7e85380c9dc4ac37d45c62c9f6dfda30d9f7b1c
SusanDz <sd2009@hw.ac.uk>
2023-04-10 17:16:16 +0400

    add a product to database and restrieve list of products according to url accessed

--- webapp/product_page/routes.py
+++ webapp/product_page/routes.py
@@ -1,10 +1,18 @@
 from flask import render_template, request, flash, redirect, url_for
 from webapp.product_page import product_page as product
+from .. import db
 
-@product.route('/products', methods=['GET', 'POST'])
+@product.route('/products', methods=['GET'])
 def products():
-    #get products from db and send to to html
-    return render_template('products.html')
+    #get products from db and send to html
+    products = db.products.find()
+
+    #get list of products from collection
+    productls = []
+    for product in products:
+        productls.append({'name': product['name'], 'price': product['price'], 'img': product['picture']})
+    
+    return render_template('products.html', products = productls, navOptions= {'/order': 'Shopping Cart'})
 
 @product.route('/addProduct', methods=['GET', 'POST'])
 def addProduct():
@@ -14,6 +22,17 @@ def addProduct():
         price = request.form.get('price')
         picture = request.form.get('picture')
 
+        # create a new product document
+        product_doc = {
+            'name': name,
+            'price': price,
+            'picture': picture
+        }
+
+        # insert the product document to the database
+        db.products.insert_one(product_doc)
+
+        # send a flash message to screen with success message
         flash('Created product succesfully!', category='success')
         
-    return render_template('addProduct.html')
+    return render_template('addProduct.html', navOptions= {'/products': 'Products'})
