ac5a7f508e27c76e2a2815c77e7e54deb1c2f7bc
SusanDz <sd2009@hw.ac.uk>
2023-04-16 13:51:28 +0400

    created model for user

---
+++ webapp/models.py
@@ -0,0 +1,25 @@
+from flask_login import UserMixin
+from webapp import db
+
+class User(UserMixin):
+    def __init__(self, _id, username, password, role, products):
+        self._id = _id
+        self.username = username
+        self.password = password
+        self.role = role
+        self.products = products
+
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    def get_id(self):
+        return str(self._id)
