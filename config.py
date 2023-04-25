82b47de2b2a72f1cfdb844bd4468cb89e0e0f137
SusanDz <sd2009@hw.ac.uk>
2023-04-25 23:34:59 +0400

    pushed file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 class Config:
     SECRET_KEY = 'Final dissertation 2023'
-    MONGO_URI = 'mongodb+srv://dev:root@cluster0.x52x0kh.mongodb.net/?retryWrites=true&w=majority'
+    # MONGO_URI = 'mongodb+srv://dev:root@cluster0.x52x0kh.mongodb.net/?retryWrites=true&w=majority'
 
     # establish connection with sqlalchemy
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///mono.db'
