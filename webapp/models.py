10696a3393767e9e480ea65bd3527c393bd96dee
SusanDz <sd2009@hw.ac.uk>
2024-03-27 13:59:43 +0400

    Incorporated pydantic library to Validate user input against a set of constraints

--- webapp/models.py
+++ webapp/models.py
@@ -1,13 +1,35 @@
 from flask_login import UserMixin
-from webapp import db
+from flask_pymongo import PyMongo
 
-class User(UserMixin):
-    def __init__(self, _id, username, password, role, products):
-        self._id = _id
-        self.username = username
-        self.password = password
-        self.role = role
-        self.products = products
+mongo = PyMongo()
+
+from pydantic import BaseModel, ConfigDict, Field
+from typing import List, Optional, TypeVar, Annotated
+from bson.objectid import ObjectId as BsonObjectId
+
+T = TypeVar('T')  # or a bound=SupportGt
+
+ObjectIdStr = List[Annotated[T, BsonObjectId]]
+
+class User(BaseModel, UserMixin):
+    _id: Optional[ObjectIdStr] = None
+    username: str = Field(min_length=4)
+    password: str = Field(min_length=8) # regex
+    role: Optional[str] = None # Literal
+    products: Optional[ObjectIdStr[BsonObjectId]] = []
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    # def __init__(self, _id, username, password, role, products):
+    #     self._id = _id
+    #     self.username = username
+    #     self.password = password
+    #     self.role = role
+    #     self.products = products
+
+    #     username = mongo.db.StringField()
+    #     password = db.StringField()
+    #     mail = db.StringField()
 
     @staticmethod
     def is_authenticated():
@@ -23,6 +45,15 @@ class User(UserMixin):
 
     def get_id(self):
         return str(self._id)
+    
+    # def to_json(self):
+    #     return jsonable_encoder(self, exclude_none=True)
+
+    def to_bson(self):
+        data = self.dict(by_alias=True, exclude_none=True)
+        if data["_id"] is None:
+            data.pop("_id")
+        return data
     
 class Product():
     def __init__(self, _id, name, price, picture):
